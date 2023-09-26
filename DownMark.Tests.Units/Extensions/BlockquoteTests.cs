6ac5e4c163b66b46ea41022e211ef54ae15bd207
Peter Bresic <19489992+Peet-HD@users.noreply.github.com>
2023-09-26 22:24:02 +0200

    Add new blockquote element

---
+++ DownMark.Tests.Units/Extensions/BlockquoteTests.cs
@@ -0,0 +1,61 @@
+﻿using DownMark.Extensions;
+
+namespace DownMark.Tests.Units.Extensions
+{
+    public class BlockquoteTests
+    {
+        [Fact]
+        public void BlockquoteTest()
+        {
+
+            var blockQuote = "Hello, this is a test.";
+            var expected = "> " + blockQuote + Environment.NewLine + Environment.NewLine;
+
+            var actual = new MarkdownBuilder()
+    .Blockquote(blockQuote)
+    .Build();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BlockquoteWithMarkdownElementsTest()
+        {
+            var blockQuote = new MarkdownBuilder()
+                .Text("Hello ")
+                .Link("https://wikipedia.org/", "Wikipedia", "This is a link to wikipedia.")
+                .Text(".");
+
+            var expected = @"> Hello 
+> [Wikipedia](https://wikipedia.org/ ""This is a link to wikipedia."")
+> .
+
+
+";
+            var actual = new MarkdownBuilder()
+                .Blockquote(blockQuote)
+                .Build();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MultilineBlockquoteTest()
+        {
+            string blockQuote = @"Hello,
+This is a test.";
+
+            var expected = $@">>>
+{blockQuote}
+>>>
+
+
+";
+            var actual = new MarkdownBuilder()
+                .MultilineBlockquote(blockQuote)
+                .Build();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
--- DownMark/Extensions/InlineExtensions.cs
+++ DownMark/Extensions/InlineExtensions.cs
@@ -16,5 +16,4 @@ namespace DownMark.Extensions
             return builder;
         }
     }
-
 }
