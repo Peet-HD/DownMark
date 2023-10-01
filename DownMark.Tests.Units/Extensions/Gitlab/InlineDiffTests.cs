fa8af1c0e59f2fc0a842436c2bd733a72b241235
Peter Bresic <19489992+Peet-HD@users.noreply.github.com>
2023-10-01 02:03:04 +0200

    Finished tests for gitlab reference

---
+++ DownMark.Tests.Units/Extensions/Gitlab/InlineDiffTests.cs
@@ -0,0 +1,31 @@
+﻿using DownMark.Extensions;
+using DownMark.Extensions.Gitlab;
+
+namespace DownMark.Tests.Units.Extensions.Gitlab
+{
+    public class GitlabInlineDiffTests
+    {
+        [Fact]
+        public void AdditionTest()
+        {
+            var text = Guid.NewGuid().ToString();
+            var expected = $"- {{+ {text} +}}" + Environment.NewLine + Environment.NewLine;
+            var actual = new MarkdownBuilder()
+                .Addition(text)
+                .Build();
+
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void DeletionTest()
+        {
+            var text = Guid.NewGuid().ToString();
+            var expected = $"- {{- {text} -}}" + Environment.NewLine + Environment.NewLine;
+            var actual = new MarkdownBuilder()
+                .Deletion(text)
+                .Build();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
