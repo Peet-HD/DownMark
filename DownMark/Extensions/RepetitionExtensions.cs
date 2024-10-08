1b84b97ecbd212de10f6efca17e1bf7fba265d74
Peet-HD <19489992+Peet-HD@users.noreply.github.com>
2024-10-08 23:02:54 +0200

    fixed repetition methods

--- DownMark/Extensions/RepetitionExtensions.cs
+++ DownMark/Extensions/RepetitionExtensions.cs
@@ -4,12 +4,18 @@ public static class RepetitionExtensions
 {
     public static MarkdownBuilder Repetition(this MarkdownBuilder builder, string text, int count = 1)
     {
-        builder.Entities.Add(text);
+        for (int i = 0; i < count; i++)
+        {
+            builder.Entities.Add(text);
+        }
         return builder;
     }
     public static MarkdownBuilder Repetition(this MarkdownBuilder builder, char character, int count = 1)
     {
-        builder.Entities.Add(character.ToString());
+        for (int i = 0; i < count; i++)
+        {
+            builder.Entities.Add(character.ToString());
+        }
         return builder;
     }
 }
